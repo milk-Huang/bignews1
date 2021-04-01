60fa80edc394b3796474b019c89f65f8044afe54
huangzuobin <2805944622@qq.com>
2021-04-01 10:16:19 +0800

    接口已完成

--- serve/router/cate_router.js
+++ serve/router/cate_router.js
@@ -4,6 +4,59 @@ const conn = require('../util/sql.js')
 const jwt = require('jsonwebtoken')
 router.use(express.urlencoded())
 
+// 获取文章分类列表
+router.get('/cates', (req, res) => {
+    // console.log('获取到的数据', req, res);
+    const selectStr = `select * from categories`
+    conn.query(selectStr, (err, result) => {
+        if (err) return res.json({ status: 501, msg: '服务器错误' })
+        res.json({ status: 200, msg: '获取成功', data: result })
+    })
+})
 
+// 新增文章分类
+router.post('/addcates', (req, res) => {
+    const { name, slug } = req.body
+    const insertStr = `insert into categories(name,slug) values("${name}","${slug}")`
+    console.log(insertStr);
+    conn.query(insertStr, (err, result) => {
+        if (err) return res.json({ status: 501, msg: '服务器错误' })
+        res.json({ status: 200, msg: '添加成功' })
+    })
+})
+
+// 根据 Id 删除文章分类
+router.get('/deletecate', (req, res) => {
+    const { id } = req.body
+    // 查询文章 id
+    const deleteStr = `delete from categories where id=${id}`
+    // console.log(deleteStr);
+    conn.query(deleteStr, (err, result) => {
+        if (err) return res.json({ status: 501, msg: '服务器错误' })
+        res.json({ status: 200, msg: '分类删除成功' })
+    })
+})
+
+// 根据 Id 获取文章分类数据
+router.get('/getCatesById', (req, res) => {
+    const { id } = req.query
+    const selectStr = `select * from categories where id=${id}`
+    console.log(selectStr);
+    conn.query(selectStr, (err, result) => {
+        if (err) return res.json({ status: 500, msg: '服务器失败' })
+        res.json({ status: 200, msg: '获取成功', data: result })
+    })
+})
+
+// 根据 Id 更新文章分类数据
+router.post('/updatecate', (req, res) => {
+    const { id, name, slug } = req.body
+    const modifyStr = `update categories set name="${name}",slug="${slug}"  where id=${id}`
+    console.log(modifyStr);
+    conn.query(modifyStr, (err, result) => {
+        if (err) return res.json({ status: 500, msg: '服务器错误' })
+        res.json({ status: 200, msg: '更新成功' })
+    })
+})
 
 module.exports = router
--- serve/router/user_router.js
+++ serve/router/user_router.js
@@ -23,6 +23,81 @@ router.get('/userinfo', (req, res) => {
 })
 
 
+// 文件上传专用
+const multer = require('multer')
+// 精细化去设置，如何去保存文件
+const storage = multer.diskStorage({
+    // 保存在哪里
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    // 保存时，文件名叫什么
+    filename: function (req, file, cb) {
+        // console.log('file', file)
+        // 目标： 新名字是时间戳+后缀名
+        const filenameArr = file.originalname.split('.');
+        // filenameArr.length-1是找到最后一个元素的下标
+        const fileName = Date.now() + "." + filenameArr[filenameArr.length - 1]
+        cb(null, fileName)
+    }
+})
+// 导出存放文件的地方
+const uploads = multer({ storage })
+
+// 上传用户头像
+router.post('/uploadPic', uploads.single('file_data'), (req, res) => {
+    // 如果文件上传成功
+    console.log('本次上传的文件是', req.file)
+    // 后面再做：错误处理
+    res.json({
+        "code": 200,
+        "msg": "上传成功",
+        "src": "http://127.0.0.1:3030/uploads/" + req.file.filename
+    })
+})
+
+
+// 更新用户
+router.post('/userinfo', (req, res) => {
+    // 解构
+    const { id, nickname, email, userPic, password } = req.body
+    // 更新sql
+    const modifyStr = `update users set nickname="${nickname}",email="${email}",userPic="${userPic}" where id="${id}"`
+    // console.log(modifyStr);
+    conn.query(modifyStr, (err, result) => {
+        // 修改错误
+        if (err) return res.json({ status: 501, msg: '服务器错误' })
+        // 成功修改
+        res.json({ status: 200, msg: '用户信息修改成功' })
+    })
+})
+
+// 重置用户密码
+router.post('/updatepwd', (req, res) => {
+    // 解构
+    const { oldPwd, newPwd, id } = req.body;
+    // return console.log(oldPwd, newPwd, id);
+    // 查询sql
+    const selStr = `select password from users where id=${id}`
+    conn.query(selStr, (err, result) => {
+        // 修改错误
+        if (err) return res.json({ status: 501, msg: '服务器错误' })
+        // result[0].password  就是原密码
+        if (result[0].password !== oldPwd) return res.json({ status: 401, msg: "旧密码错误" })
+
+        // 修改密码sql
+        const modifyStr = `update users set password="${newPwd}" where id="${id}"`
+        // console.log(modifyStr);
+        conn.query(modifyStr, (err, result) => {
+            // 修改错误
+            if (err) return res.json({ status: 501, msg: '服务器错误' })
+            // 成功修改
+            res.json({ status: 200, msg: '用户密码修改成功' })
+        })
+
+    })
+})
+
 
 
 
